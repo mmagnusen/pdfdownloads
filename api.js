93d3c22ab30a5aaf731ae6c12911ab10ada58b55
Marilyn Magnusen <m-mags@hotmail.co.uk>
2021-03-04 23:24:11 +0000

    add express as dep

--- api.js
+++ api.js
@@ -1,15 +1,26 @@
+const express = require('express');
 const puppeteer = require('puppeteer');
+const app = express();
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1440, height: 900, deviceScaleFactor: 2 });
-    await page.goto('https://www.headstack.co', { waitUntil: 'networkidle2' });
-    await page.pdf({
-        path: 'resume.pdf',
-        format: 'A4',
-        printBackground: true
-    });
+app.get('/', function (req, res) {
+    (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1440, height: 900, deviceScaleFactor: 2 });
+        await page.goto('https://www.headstack.co', { waitUntil: 'networkidle2' });
+        await page.pdf({
+            path: './downloads/resume.pdf',
+            format: 'A4',
+            printBackground: true
+        });
 
-    await browser.close();
-})();
+        await browser.close();
+    })();
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.send('Hellow World, this is express')
+})
+
+app.listen(3000, () => {
+    console.log('server started')
+})
